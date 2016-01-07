faa42ba9fa1ebc2a878287ac2e7d0fd67812b2f0
Yusef Napora <yusef@napora.org>
2016-01-07 17:43:31 -0500

    memoize gray code conversion

--- src/ow/routing/chord/GrayCode.java
+++ src/ow/routing/chord/GrayCode.java
@@ -1,6 +1,9 @@
 package ow.routing.chord;
 
 import java.math.BigInteger;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 /**
  * Utility functions for converting to/from binary and Gray Code (reflected binary).
@@ -10,19 +13,44 @@ import java.math.BigInteger;
  *  That may not be an issue for our domain; if so we should at least
  *  throw if we get a negative number.
  */
+
 public class GrayCode {
+
+  private static class LRUCache extends LinkedHashMap<BigInteger, BigInteger> {
+    int cacheSize;
+
+    LRUCache(int cacheSize) {
+      super(16, 0.75f, true);
+      this.cacheSize = cacheSize;
+    }
+
+    protected boolean removeEldestEntry(Map.Entry<BigInteger, BigInteger> eldest) {
+      return size() >= cacheSize;
+    }
+  }
+
+  // BigInteger conversion from gray code to binary is crazy slow.
+  private static final LRUCache fromGrayMemo = new LRUCache(65536);
 
   public static BigInteger toGray(BigInteger n) {
     return n.xor(n.shiftRight(1));
   }
 
   public static BigInteger fromGray(BigInteger n) {
+    if (fromGrayMemo.containsKey(n)) {
+      return fromGrayMemo.get(n);
+    }
+
+    BigInteger orig = n;
     for (BigInteger i = n.shiftRight(1);
          i.compareTo(BigInteger.ZERO) != 0;
          n = n.xor(i), i = i.shiftRight(1)) {
       // ;
     }
 
+    synchronized (fromGrayMemo) {
+      fromGrayMemo.put(orig, n);
+    }
     return n;
   }
 
